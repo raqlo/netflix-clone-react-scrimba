a4e2a972c0ffa2867ce72c26cd66df179730d4a2
Raquel López <raquelcle@gmail.com>
2021-12-16 22:05:46 -0400

    Add and style form.js

--- src/app.js
+++ src/app.js
@@ -1,25 +1,25 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import * as ROUTES from'./constants/routes'
-import Home from './pages/home';
+import * as ROUTES from './constants/routes';
+import { Home, Signin } from './pages';
 
 export function App() {
-  return (
-    <Router>
-        <Switch>
-            <Route path={ROUTES.SIGN_IN}>
-                <p>I will be the sign in page</p>
-            </Route>
-            <Route path={ROUTES.SIGN_UP}>
-                <p>I will be the sign up page</p>
-            </Route>
-            <Route path={ROUTES.BROWSE}>
-                <p>I will be the browse page</p>
-            </Route>
-            <Route path={ROUTES.HOME}>
-                <Home />
-            </Route>
-        </Switch>
-    </Router>
-  );
+    return (
+        <Router>
+            <Switch>
+                <Route path={ROUTES.SIGN_IN}>
+                    <Signin />
+                </Route>
+                <Route path={ROUTES.SIGN_UP}>
+                    <p>I will be the sign up page</p>
+                </Route>
+                <Route path={ROUTES.BROWSE}>
+                    <p>I will be the browse page</p>
+                </Route>
+                <Route path={ROUTES.HOME}>
+                    <Home />
+                </Route>
+            </Switch>
+        </Router>
+    );
 }
--- src/index.js
+++ src/index.js
@@ -2,11 +2,27 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { GlobalStyles } from './global-styles';
 import { App } from './app';
+import { FirebaseContext } from './context/firebase';
+
+const config = {
+    apiKey: "AIzaSyC1mqqGsn43PDaf4Ik-nD_emfL0fW6WtwM",
+    authDomain: "netflix-clone-11e4e.firebaseapp.com",
+    databaseURL: "https://netflix-clone-11e4e-default-rtdb.firebaseio.com",
+    projectId: "netflix-clone-11e4e",
+    storageBucket: "netflix-clone-11e4e.appspot.com",
+    messagingSenderId: "609918165092",
+    appId: "1:609918165092:web:27286faedbf1a249817f71",
+    measurementId: "G-9PP8QYP517"
+}
+
+const firebase = window.firebase.initializeApp(config);
 
 ReactDOM.render(
     <>
-        <GlobalStyles />
-        <App />
-    </>, 
+        <FirebaseContext.Provider value={{ firebase: window.firebase }}>
+            <GlobalStyles />
+            <App />
+        </FirebaseContext.Provider>
+    </>,
     document.getElementById('root')
 );
